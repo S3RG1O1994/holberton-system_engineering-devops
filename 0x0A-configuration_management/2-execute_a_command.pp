e1336eac2e890db975df337dd31e992544f3b55f
S3RG1O1994 <se.diaz60@gmail.com>
2020-08-03 16:13:51 -0500

    [Fix]: New correction about dir and flags in command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # /etc/puppet/manifests/site.pp
 
 exec { 'pkill killmenow':
-  path    => 'usr/bin',
-  command => 'pkill killmenow',
+  path    => '/usr/bin',
+  command => 'pkill -x killmenow',
 }
