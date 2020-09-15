0c1020ba101da257f48069d3104b0aa51ce510d9
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-14 23:19:37 -0500

    [Add]: New task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,18 +11,17 @@ if __name__ == '__main__':
     name = requests.get(URL + '/users/').json()
     new_dict = {}
     obj_j = {}
-    new_list = []
     for user in name:
         un = user.get('username')
         UID = user.get('id')
         todo = requests.get(URL + '/todos?userId={}'.format(UID)).json()
+        new_list = []
         for item in todo:
             new_dict = {
                 'username': un,
                 'task': item.get('title'),
-                'completed': item.get('completed'),
-            }
+                'completed': item.get('completed')}
             new_list.append(new_dict)
-        obj_j[UID] = new_list
+        obj_j[str(UID)] = new_list
     with open('todo_all_employees.json', mode='w') as json_file:
         json.dump(obj_j, json_file)
