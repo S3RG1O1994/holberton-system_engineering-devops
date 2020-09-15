3421111344f154b72c63b375d77be3c0248487cc
S3RG1O1994 <se.diaz60@gmail.com>
2020-09-14 19:28:23 -0500

    [Fix]: mode of save information in file.csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,16 +7,10 @@ from sys import argv
 
 if __name__ == '__main__':
     URL = 'https://jsonplaceholder.typicode.com'
-    name = requests.get(URL + '/users/' + argv[1]).json()['name']
+    name = requests.get(URL + '/users/' + argv[1]).json()['username']
     todo = requests.get(URL + '/todos?userId=' + argv[1]).json()
-    total_task = [(item) for item in todo]
-    file = [
-        total_task[int(argv[1])]['userId'],
-        name,
-        total_task[int(argv[1])]['completed'],
-        total_task[int(argv[1])]['title']
-    ]
-    print(str(file[0]))
     with open('{}.csv'.format(argv[1]), mode='w') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writerow(file)
+        for item in todo:
+            writer.writerow([int(argv[1]), name,
+                item.get('completed'), item.get('title')])
